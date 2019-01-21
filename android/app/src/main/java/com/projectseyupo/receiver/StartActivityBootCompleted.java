1f2fd9e5a58061c254aece1ff67a88fdfd35a940
cunduts22 <yusrilsabir22@gmail.com>
2019-01-21 16:47:51 +0700

    added layout add device

---
+++ android/app/src/main/java/com/projectseyupo/receiver/StartActivityBootCompleted.java
@@ -0,0 +1,21 @@
+package com.projectseyupo.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.facebook.react.HeadlessJsTaskService;
+
+import com.projectseyupo.MainActivity;
+
+public class StartActivityBootCompleted extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Intent i = new Intent(context, MainActivity.class);
+            i.addFlags(intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+            HeadlessJsTaskService.acquireWakeLockNow(context);
+        }
+    }
+}
